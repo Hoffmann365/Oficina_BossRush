b890513ff97c8fb5459bf968b3780eed233588ba
KARLA JULIANE SANTOS MEIRA <j.karla@academico.ifrn.edu.br>
2023-11-06 16:07:36 -0300

    scripts dano e ataque

--- Game_BossRush/Assets/scripts/Boss3.cs
+++ Game_BossRush/Assets/scripts/Boss3.cs
@@ -1,40 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Boss3 : MonoBehaviour
 {
-    public Transform player;
+    public float speed;
+    public float walkTime;
+    public float life = 40f;
+    public float currentHealth;
 
-    public bool isFlipped = false;
+    public int health;
 
-    public void LookAtPlayer()
-    {
-        Vector3 flipped = transform.localScale;
-        flipped.z *= -1f;
+    private float timer;
+    private bool walkRight;
 
-        if (transform.position.x > player.position.x && isFlipped)
-        {
-            transform.localScale = flipped;
-            transform.Rotate(0f, 180f, 0f);
-            isFlipped = false;
-        }
-        else if (transform.position.x < player.position.x && !isFlipped)
-        {
-            transform.localScale = flipped;
-            transform.Rotate(0f, 180f, 0f);
-            isFlipped = true;
-        }
-    }
+    private Rigidbody2D rig;
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateHealthBar();
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        timer += Time.deltaTime;
         
+        if(timer >= walkTime)
+        {
+            walkRight = !walkRight;
+            timer = 0f;
+        }
+        if (walkRight)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+            rig.velocity = Vector2.right * speed;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            rig.velocity = Vector2.left * speed;
+        }
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Flecha")
+        {
+            life--;
+            currentHealth = Math.Clamp(currentHealth, 0f, life);
+            UpdateHealthBar();
+        }
+
+        if (col.gameObject.tag == "RaioPlayer")
+        {
+            life -= 2;
+            currentHealth = Math.Clamp(currentHealth, 0f, life);
+            UpdateHealthBar();
+        }
+
+        void dead()
+        {
+            if (life <= 0)
+            {
+                Isdead = true;
+                Animation.SetTrigger("Dead");
+                Destroy(GetComponent<Rigidbody2D>());
+                Destroy(GetComponent<BoxCollider>());
+                Destroy(gameObject, 5f);
+            }
+        }
     }
 }
---
+++ Game_BossRush/Assets/scripts/introBehaviour.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introBehaviour : StateMachineBehaviour
+{
+    private int rand;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        rand = Random.Range(0, 2);
+
+        if (rand == 0)
+        {
+            animator.SetTrigger("Walk");
+        }
+        else
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+
+    
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+    }
+
+    
+}
