b81d8193c3c4da8c929b974c19cb103c372c18d6
KARLA JULIANE SANTOS MEIRA <j.karla@academico.ifrn.edu.br>
2023-11-07 16:39:31 -0300

    mais uma tentativa triste

--- Game_BossRush/Assets/scripts/Boss3.cs
+++ Game_BossRush/Assets/scripts/Boss3.cs
@@ -7,18 +7,26 @@ using UnityEngine;
 public class Boss3 : MonoBehaviour
 {
     public float speed = 2.0f;
+    public float walkTime;
+    public bool walkRight = true;
     public float attackRange = 2.0f;
     public float attackCooldown = 2.0f;
     
     public int health;
+
     private bool enraged;
     private Transform playerTransform;
     private float lastAttackTime;
+    private float timer;
+    private bool isAtk;
+    
+    private Rigidbody2D rig;
 
     private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
+        rig = GetComponent<Rigidbody2D>();
         health = 100;
         enraged = false;
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
@@ -49,19 +57,37 @@ public class Boss3 : MonoBehaviour
             //comportamento padrão
             Wander();
         }
+        AttackPlayer();
     }
 
     private void Enrage()
     {
+        anim.SetTrigger("Run");
         enraged = true;
         //mudar o comportamento após ficar enfurecido
     }
 
     private void Wander()
     {
-        //Lógica de movimento padrão 
-        Vector3 direction = (playerTransform.position - transform.position).normalized;
-        transform.position += direction * speed * Time.deltaTime;
+        anim.SetTrigger("Walk");
+        timer += Time.deltaTime;
+
+        if (timer >= walkTime)
+        {
+            walkRight = !walkRight;
+            timer = 0f;
+        }
+
+        if (walkRight)
+        {
+            transform.eulerAngles = new Vector3(0, 0);
+            rig.velocity = Vector3.right * speed;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 180);
+            rig.velocity = Vector3.left * speed;
+        }
     }
 
     private void AttackPlayer()
@@ -69,15 +95,20 @@ public class Boss3 : MonoBehaviour
         //Lógica de ataque agressivo
         if (Vector3.Distance(transform.position, playerTransform.position) <= attackRange)
         {
-            //StartCoroutine("ATK");
+            StartCoroutine("ATK");
         }
     }
 
-    /*IEnumerator ATK()
+    IEnumerator ATK()
     {
-        anim.SetTrigger("Attack");
-        yield return new WaitForSeconds(1f);
-    }*/
+        if (Vector3.Distance(transform.position, playerTransform.position) <= attackRange)
+        {
+            isAtk = true;
+            anim.SetTrigger("Attack");
+            yield return new WaitForSeconds(1f);
+        }
+        
+    }
 
     private bool CanAttack()
     {
