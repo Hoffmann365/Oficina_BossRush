9e1e66629210b3d455f1642f7af622a2e5d15d63
KARLA JULIANE SANTOS MEIRA <j.karla@academico.ifrn.edu.br>
2023-11-08 11:58:11 -0300

    boss health (falta ataque e dano)

---
+++ Game_BossRush/Assets/Boss_Walk.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Boss_Walk : StateMachineBehaviour
+{
+    public float speed = 2.5f;
+    public float attackRange = 3f;
+    
+    private Transform player;
+    private Rigidbody2D rb;
+    private Boss3 boss;
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<Boss3>();
+    }
+
+    
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss.LookAtPlayer();
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+        
+        if (Vector2.Distance(player.position, rb.position) <= attackRange)
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+
+    
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.ResetTrigger("Attack");
+    }
+
+
+}
--- Game_BossRush/Assets/scripts/Boss3.cs
+++ Game_BossRush/Assets/scripts/Boss3.cs
@@ -1,132 +1,29 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class Boss3 : MonoBehaviour
 {
-    public float speed = 2.0f;
-    public float walkTime;
-    public bool walkRight = true;
-    public float attackRange = 2.0f;
-    public float attackCooldown = 2.0f;
+    public Transform player;
+
+    public bool isFlipped = false;
     
-    public int health;
-
-    private bool enraged;
-    private Transform playerTransform;
-    private float lastAttackTime;
-    private float timer;
-    private bool isAtk;
-    
-    private Rigidbody2D rig;
-
-    private Animator anim;
-    // Start is called before the first frame update
-    void Start()
+    public void LookAtPlayer()
     {
-        rig = GetComponent<Rigidbody2D>();
-        health = 100;
-        enraged = false;
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-        lastAttackTime = Time.time;
-        anim = GetComponent<Animator>();
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
 
-    }
-
-    // Update is called once per frame
-    public void Update()
-    {
-        if (health <= 50 && !enraged)
+        if (transform.position.x > player.position.x && isFlipped)
         {
-            Enrage();
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
         }
-
-        if (enraged)
+        else if (transform.position.x < player.position.x && !isFlipped)
         {
-            if (CanAttack())
-            {
-                //comportamento agressivo
-                AttackPlayer();
-            }
-            
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
         }
-        else
-        {
-            //comportamento padrão
-            Wander();
-        }
-        AttackPlayer();
-    }
-
-    private void Enrage()
-    {
-        anim.SetTrigger("Run");
-        enraged = true;
-        //mudar o comportamento após ficar enfurecido
-    }
-
-    private void Wander()
-    {
-        anim.SetTrigger("Walk");
-        timer += Time.deltaTime;
-
-        if (timer >= walkTime)
-        {
-            walkRight = !walkRight;
-            timer = 0f;
-        }
-
-        if (walkRight)
-        {
-            transform.eulerAngles = new Vector3(0, 0);
-            rig.velocity = Vector3.right * speed;
-        }
-        else
-        {
-            transform.eulerAngles = new Vector3(0, 180);
-            rig.velocity = Vector3.left * speed;
-        }
-    }
-
-    private void AttackPlayer()
-    {
-        //Lógica de ataque agressivo
-        if (Vector3.Distance(transform.position, playerTransform.position) <= attackRange)
-        {
-            StartCoroutine("ATK");
-        }
-    }
-
-    IEnumerator ATK()
-    {
-        if (Vector3.Distance(transform.position, playerTransform.position) <= attackRange)
-        {
-            isAtk = true;
-            anim.SetTrigger("Attack");
-            yield return new WaitForSeconds(1f);
-        }
-        
-    }
-
-    private bool CanAttack()
-    {
-        return (Time.time - lastAttackTime) >= attackCooldown;
-    }
-
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("Player"))
-        {
-            //lógica de colisão com o jogador
-            int damage = 10;
-            TakeDamage(damage);
-        }
-    }
-
-    private void TakeDamage(int damage)
-    {
-        health -= damage;
     }
 }
